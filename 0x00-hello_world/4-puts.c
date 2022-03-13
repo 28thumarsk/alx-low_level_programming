c59e0e1a94a4b292ed16c065ae22fee97cfb5514
28thumarsk <praisekanisuru999@gmail.com>
2022-03-13 21:55:49 +0000

    Task 4 -- A script that prints exactly "Programming is like building a multilingual puzzle" , followed by a new line.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,7 @@
  *   * Description: prints "Programming is like building a multilingual puzzle"
  *    *
  *     * Return: Always 0 (Success) **/
-int main(void)
-{
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+int main(void) {
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
