48fcb218dbe36843622a764c45c1c5a10c2d5530
28thumarsk <praisekanisuru999@gmail.com>
2022-04-01 16:17:31 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - prints the argument count
+* @argc: offset count
+* @argv: offset value
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+(void)argv;
+return (0);
+}
