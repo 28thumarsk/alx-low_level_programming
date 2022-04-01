38a6e5d5b9ec73451e2062458cf00c5d891709f1
28thumarsk <praisekanisuru999@gmail.com>
2022-04-01 16:15:27 +0000

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - prints the name of the file
+* @argc: offset count
+* @argv: offset value
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
